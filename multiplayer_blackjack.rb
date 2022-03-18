352acb906cbc17f1b6e0b353e3357eafcbbe3fef
Joe <j.newman@live.co.uk>
2022-03-18 11:12:50 +0000

    Rearranged methods and added missing -10 for some ace conditions

--- multiplayer_blackjack.rb
+++ multiplayer_blackjack.rb
@@ -20,20 +20,135 @@
     "jack of diamonds", "queen of diamonds", 
     "king of diamonds", "ace of diamonds"
   ]
-  @game_count = 0 
+@game_count = 0 
 
-  def move
-  loop do
-    ans = gets.chomp
-    if ans == "hit" || ans == "h" || ans == "Hit" || ans == "HIT"
-      return "hit"
-    elsif ans == "stick" || ans == "s" || ans == "Stick" || ans == "STICK"
-      return "stick"
+def welcome
+  message = [
+    "\nWelcome to Mika's Casino BlackJack Table.", "Aces are wild of course - why wouldn't they be?", "Five cards in your (virtual) hand trumps 21.", 
+    "And six cards in your hand is better than five et cetera, et cetera.", "Feeling lucky?\n\n", "How close can you get to 21?", "Can you get to five cards?", 
+    "To start, you want to get as close to 21 in as few cards as possible.", "But if you get to four cards, you might try to hold on for five.", "Weird, right?",
+    "Over 16 with the best hand could squeeze out a win, but we all know what we're playing for here.\n\n", 
+    "This is cards out blackjack - everybody can see every card.", "We proceed one card per player at a time.\n\n", 
+    "If you want one player blackjack quit now (control + c), and try our solo Blackjack table.\n\n\n",  
+    "You can use the shortcut [h] for 'hit' and the shortcut [s] for 'stick'.",
+    "Control + c at any time to quit.", "Make sure you do quit when you're done, otherwise the computer will be stuck in an infinite loop.",
+    "Infinite loop = tired (and warm) computer\n\n\n\n",
+    ]
+    if @game_count == 0
+      message.each do |sentence|
+        puts sentence
+        sleep(1.5)
+      end
     else
-      sleep(1)
-      puts "Type in 'hit or 'stick'. Or just use 'h' or 's' if that seems like too much work."
+      puts "Great, let's play again!\n\n"
+      sleep(1.5)
     end
+    @game_count += 1
+end
+  
+def make_players
+  sleep(1)
+  puts "How many players? Maximum 10. Type the number.\n\n"
+  players = nil
+  index = 0
+  
+  loop do
+    players  = gets.chomp
+    break if number?(players) && players.to_i < 11 && players.to_i > 1
+    puts "Enter a number. 2, 3, 4, 5 etc, up to 10. Get it?"
   end
+  
+  sleep(0.5)
+  puts "\nWell done, that's a valid number of players!\n\n"
+  num = players.to_i - 1
+
+  (0..num).each do |name|
+    puts "\nPlayer #{index+1} - what's your name?"
+    name = gets.chomp.capitalize
+    sleep(0.5)
+    @in_play << Array.new(1, name)
+    index += 1
+  end
+  puts "\n\n\n"
+end
+
+def number?(obj)
+  obj = obj.to_s unless obj.is_a? String
+  /\A[+]?\d+\z/.match(obj)
+end
+
+def deal
+  @in_play.each do |a|
+    sleep(1.5)
+    a.push(@ran_cards.pop).push(@ran_cards.pop)
+    puts "\n\n#{a[0]} was dealt the #{a[1]} and the #{a[2]}. Their score so far is #{score(a)}."
+    puts "#{a[0]} could of course think of their score as #{score(a)-10} if they prefer, as they have an ace to play with.\n" if @ace_count > 0
+    @ace_count -= @ace_count
+  end
+  puts "\n\n"
+end
+
+def turns
+  sleep(2)
+  puts "\n\n\n"
+  
+  while @in_play.size > 0 do
+    loop do
+      break if @in_play.size == 0
+      if score(@in_play[@i]) > 20 && @ace_count == 0
+        puts "\n\n\n#{@in_play[@i][0]},  got 21 from 2 cards, so they've probably won. They can only be beaten by a trick hand. They'll have to stop there anyway.\n\n"
+        add_final(@in_play[@i])
+        next_player
+      else
+        break
+      end
+    end
+
+    if @in_play.size == 1
+      puts "\n\n\n#{@in_play[@i][0]} is the last player in the game. #{score(@in_play[@i])} so far, from #{@in_play[@i].size - 1} cards. Hit or stick?"
+      puts "Remember #{@in_play[@i][0]}, has an ace to play with, so their score could also be considered to be #{score(@in_play[@i]) - 10}." if @ace_count > 0
+    elsif @in_play.size > 1
+      puts "\n\n\nIt's #{@in_play[@i][0]}'s turn. #{score(@in_play[@i])} so far, from #{@in_play[@i].size - 1} cards. Hit or stick?"
+      puts "Remember #{@in_play[@i][0]}, has an ace to play with, so their score could also be considered to be #{score(@in_play[@i]) - 10}." if @ace_count > 0
+    else 
+      break
+    end
+
+    case 
+    when move == "hit"
+      sleep(1.5)
+      @in_play[@i].push(@ran_cards.pop)
+      puts "#{@in_play[@i][0]} got the #{@in_play[@i][-1]}."
+      if score(@in_play[@i]) > 21
+        puts "Whoops, #{@in_play[@i][0]} busted with #{score(@in_play[@i])}."
+        puts  "And before you ask #{@in_play[@i][0]}, you can't do any fiddling with aces." if @ace_count > 0
+        @busted.push(@in_play[@i][0])
+        @in_play.delete_at(@i)
+        @i -= 1
+      elsif score(@in_play[@i]) == 21
+        puts "That's 21, #{@in_play[@i][0]} may well have won. It's looking good!"
+        @ace_count > 0 ? "Because you have aces to play with #{@in_play[@i][0]}, you can have another turn in a moment, in case you're feeling courageous (i.e. crazy)." : add_final(@in_play[@i])
+      else
+        puts "#{@in_play[@i][0]}'s score so far is #{score(@in_play[@i])}, from #{@in_play[@i].size - 1} cards."
+        puts "You could of course think of your score as #{score(@in_play[@i]) - 10}, #{@in_play[@i][0]}, as you have an ace still to play with." if @ace_count > 0
+      end
+    else
+      if @in_play[@i].size > 5
+        puts "#{@in_play[@i][0]} has a #{@in_play[@i].size - 1} card trick. Looking pretty good for the win."
+        puts "You could even have held on a little longer, #{@in_play[@i][0]}, as you had an ace still to play with. What could have been!" if @ace_count > 0
+        add_final(@in_play[@i])
+      elsif score(@in_play[@i]) < 16
+        puts "#{score(@in_play[@i])} isn't enough #{@in_play[@i][0]}, you can't win. You can have another chance to be brave on your next go."
+        puts "And you could even think of your score as #{score(@in_play[@i]) - 10}, #{@in_play[@i][0]}, as you have an ace still to play with. Show some mettle!" if @ace_count > 0
+      else
+        puts "#{@in_play[@i][0]}'s final score is #{score(@in_play[@i])} from #{@in_play[@i].size - 1} cards. We'll see if that's enough in a moment."
+        puts "Will you regret not sticking with it #{@in_play[@i][0]}? You still had an ace to play with. We shall see..." if @ace_count > 0
+        add_final(@in_play[@i])
+      end
+    end
+    next_player
+  end
+  @places_index = @final.size
 end
 
 def score(hand)
@@ -87,144 +202,6 @@ def score(hand)
 
 end
 
-def number?(obj)
-  obj = obj.to_s unless obj.is_a? String
-  /\A[+]?\d+\z/.match(obj)
-end
-
-def welcome
-  message = [
-    "\nWelcome to Mika's Casino BlackJack Table.", "Aces are wild of course - why wouldn't they be?", "Five cards in your (virtual) hand trumps 21.", 
-    "And six cards in your hand is better than five et cetera, et cetera.", "Feeling lucky?\n\n", "How close can you get to 21?", "Can you get to five cards?", 
-    "To start, you want to get as close to 21 in as few cards as possible.", "But if you get to four cards, you might try to hold on for five.", "Weird, right?",
-    "Over 16 with the best hand could squeeze out a win, but we all know what we're playing for here.\n\n", 
-    "This is cards out blackjack - everybody can see every card.", "We proceed one card per player at a time.\n\n", 
-    "If you want one player blackjack quit now (control + c), and try our solo Blackjack table.\n\n\n",  
-    "You can use the shortcut [h] for 'hit' and the shortcut [s] for 'stick'.",
-    "Control + c at any time to quit.", "Make sure you do quit when you're done, otherwise the computer will be stuck in an infinite loop.",
-    "Infinite loop = tired (and warm) computer\n\n\n\n",
-    ]
-    if @game_count == 0
-      message.each do |sentence|
-        puts sentence
-        sleep(1.5)
-      end
-    else
-      puts "Great, let's play again!\n\n" if @game_count > 0
-      sleep(1.5)
-    end
-    @game_count += 1
-end
-
-def close
-  sleep(5)
-  puts "\n\nI guess you'll be playing again?\n\n"
-  sleep(2)
-  puts "Control + c to quit (if you're boring)\nAny other key to continue"
-  again = gets.chomp
-end
-
-def make_players
-  sleep(1)
-  puts "How many players? Maximum 10. Type the number.\n\n"
-  players = nil
-index = 0
-  
-  loop do
-    players  = gets.chomp
-    break if number?(players) && players.to_i < 11 && players.to_i > 1
-    puts "Enter a number. 2, 3, 4, 5 etc, up to 10. Get it?"
-  end
-  
-  sleep(0.5)
-  puts "\nWell done, that's a valid number of players!\n\n"
-  num = players.to_i - 1
-
-  (0..num).each do |name|
-    puts "\nPlayer #{index+1} - what's your name?"
-    name = gets.chomp.capitalize
-    sleep(0.5)
-    @in_play << Array.new(1, name)
-    index += 1
-  end
-  puts "\n\n\n"
-end
-
-def deal
-  @in_play.each do |a|
-    sleep(1.5)
-    a.push(@ran_cards.pop).push(@ran_cards.pop)
-    puts "\n#{a[0]} was dealt the #{a[1]} and the #{a[2]}. Their score so far is #{score(a)}.\n\n"
-    puts "#{a[0]} could of course think of their score as #{score(a)-10} if they prefer, as they have an ace to play with.\n\n" if @ace_count > 0
-    @ace_count -= @ace_count
-  end
-  puts "\n\n"
-end
-
-def turns
-  sleep(2)
-  puts "\n\n\n"
-  @i = 0
-  
-  while @in_play.size > 0 do
-    loop do
-      break if @in_play.size == 0
-      if score(@in_play[@i]) > 20 && @ace_count == 0
-        puts "#{@in_play[@i][0]},  got 21 from 2 cards, so they've probably won. They can only be beaten by a trick hand. They'll have to stop there anyway.\n\n"
-        add_final(@in_play[@i])
-        next_player
-      else
-        break
-      end
-    end
-
-    if @in_play.size == 1
-      puts "#{@in_play[@i][0]} is the last player in the game. #{score(@in_play[@i])} so far, from #{@in_play[@i].size - 1} cards. Hit or stick?"
-      puts "Remember #{@in_play[@i][0]}, has an ace to play with, so their score could also be considered to be #{score(@in_play[@i]) - 10}." if @ace_count > 0
-    elsif @in_play.size > 1
-      puts "It's #{@in_play[@i][0]}'s turn. #{score(@in_play[@i])} so far, from #{@in_play[@i].size - 1} cards. Hit or stick?"
-      puts "Remember #{@in_play[@i][0]}, has an ace to play with, so their score could also be considered to be #{score(@in_play[@i]) - 10}." if @ace_count > 0
-    else 
-      break
-    end
-
-    case 
-    when move == "hit"
-      sleep(1.5)
-      @in_play[@i].push(@ran_cards.pop)
-      puts "\n#{@in_play[@i][0]} got the #{@in_play[@i][-1]}."
-      if score(@in_play[@i]) > 21
-        puts "Whoops, #{@in_play[@i][0]} busted with #{score(@in_play[@i])}.\n\n"
-        puts  "And before you ask #{@in_play[@i][0]}, you can't do any fiddling with aces.\n\n" if @ace_count > 0
-        @busted.push(@in_play[@i][0])
-        @in_play.delete_at(@i)
-        @i -= 1
-      elsif score(@in_play[@i]) == 21
-        puts "That's 21, #{@in_play[@i][0]} may well have won. It's looking good!\n\n"
-        @ace_count > 0 ? "Because you have aces to play with #{@in_play[@i][0]}, you can have another turn in a moment, in case you're feeling courageous (i.e. crazy).\n\n" : add_final(@in_play[@i])
-      else
-        puts "#{@in_play[@i][0]}'s score so far is #{score(@in_play[@i])}, from #{@in_play[@i].size - 1} cards.\n\n"
-        puts "You could of course think of your score as #{score(@in_play[@i])}, #{@in_play[@i][0]}, as you have an ace still to play with.\n\n" if @ace_count > 0
-      end
-    else
-      if @in_play[@i].size > 5
-        puts "#{@in_play[@i][0]} has a #{@in_play[@i].size - 1} card trick. Looking pretty good for the win.\n\n"
-        puts "You could even have held on a little longer, #{@in_play[@i][0]}, as you had an ace still to play with. What could have been!\n\n" if @ace_count > 0
-        add_final(@in_play[@i])
-      elsif score(@in_play[@i]) < 16
-        puts "#{score(@in_play[@i])} isn't enough #{@in_play[@i][0]}, you can't win. You can have another chance to be brave on your next go."
-        puts "And you could even think of your score as #{score(@in_play[@i])}, #{@in_play[@i][0]}, as you have an ace still to play with. Show some mettle!\n\n" if @ace_count > 0
-      else
-        puts "#{@in_play[@i][0]}'s final score is #{score(@in_play[@i])} from #{@in_play[@i].size - 1} cards. We'll see if that's enough in a moment.\n\n"
-        puts "Will you regret not sticking with it #{@in_play[@i][0]}? You still had an ace to play with. We shall see...\n\n" if @ace_count > 0
-        add_final(@in_play[@i])
-      end
-    end
-    next_player
-  end
-  @places_index = @final.size
-end
-
 def add_final(player)
   @final.push(player)
   @in_play.delete(player)
@@ -237,6 +214,25 @@ def next_player
   if @i > (@in_play.size - 1)
     @i -= @i
   end
+end
+
+def move
+  loop do
+    ans = gets.chomp
+    if ans == "hit" || ans == "h" || ans == "Hit" || ans == "HIT"
+      return "hit"
+    elsif ans == "stick" || ans == "s" || ans == "Stick" || ans == "STICK"
+      return "stick"
+    else
+      sleep(1)
+      puts "Type in 'hit or 'stick'. Or just use 'h' or 's' if that seems like too much work."
+    end
+  end
+end
+
+def final_scores
+  final_scores_to_places_hash
+  positions_message
 end
 
 def final_scores_to_places_hash
@@ -346,22 +342,8 @@ def final_scores_to_places_hash
   end
 end
 
-class Integer
-  def ordinal
-    if self == 1
-      "1st"
-    elsif self == 2
-      "2nd"
-    elsif self == 3
-      '3rd'
-    else
-      "#{self}th"
-    end
-  end
-end
-
 def positions_message
-  puts "Tension builds as we add the scores.....\n\n\n"
+  puts "\n\n\nTension builds as we add the scores.....\n\n\n"
   sleep(6)
   puts "That took surprisingly long for a computer to do didn't it?\n\n"
   sleep(1)
@@ -369,7 +351,7 @@ def positions_message
   sleep(1)
   
   if @busted.size > 0
-    puts "Let's get the awkward bit out of the way first..."
+    puts "Let's get the awkward bit out of the way first...\n\n"
     sleep(2)
     case
     when @busted.size > 2
@@ -389,9 +371,9 @@ def positions_message
     if array.size > 0
       @places_index -= (array.size - 1)
       if @places_index == 1
-        puts "Some might say that all the placings we've mentioned so far were meaningless. Not us though..."
+        puts "Some might say that all the placings we've mentioned so far were meaningless. Not us though...\n\n"
         sleep(3)
-        puts "And the moment that we've all been waiting for.."
+        puts "And the moment that we've all been waiting for..\n\n"
         sleep(3)
         print "Victory goes to......     "
         sleep(3)
@@ -409,9 +391,26 @@ def positions_message
   end 
 end
 
-def final_scores
-  final_scores_to_places_hash
-  positions_message
+class Integer
+  def ordinal
+    if self == 1
+      "1st"
+    elsif self == 2
+      "2nd"
+    elsif self == 3
+      '3rd'
+    else
+      "#{self}th"
+    end
+  end
+end
+
+def close
+sleep(5)
+puts "\n\nI guess you'll be playing again?\n\n"
+sleep(2)
+puts "Control + c to quit (if you're boring)\nAny other key to continue"
+again = gets.chomp
 end
 
 def run_game
@@ -419,6 +418,7 @@ def run_game
   @final = []
   @busted = []
   @ran_cards = @cards.shuffle
+  @i = 0
   @places = {
     '16, 4 cards' => [], '16, 3 cards' => [], '16, 2 cards' => [], 
     '17, 4 cards' => [], '17, 3 cards' => [], '17, 2 cards' => [], 
